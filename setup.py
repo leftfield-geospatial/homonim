b954a00f2054b1fbd45ab183c331e646fd02fc06
Dugal Harris <dugalh@gmail.com>
2022-07-12 17:50:30 +0200

    update setup to specify RST description format

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     version=__version__,
     description='Correct aerial and satellite imagery to approximate surface reflectance.',
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type='text/x-rst',
     author='Dugal Harris',
     author_email='dugalh@gmail.com',
     url='https://github.com/dugalh/homonim',
