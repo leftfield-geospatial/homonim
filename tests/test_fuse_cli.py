33bd12b83fa5071437da7aae09c8deb52bea3567
Dugal Harris <dugalh@gmail.com>
2022-11-02 16:07:46 +0200

    fix compare file exists test for rasterio exception

--- tests/test_fuse_cli.py
+++ tests/test_fuse_cli.py
@@ -159,7 +159,7 @@ def test_compare_file_exists_error(runner: CliRunner, ref_file_100cm_float, src_
     cli_str = f'fuse  {src_file} {ref_file} --compare unknown.tif'
     result = runner.invoke(cli, cli_str.split())
     assert (result.exit_code != 0)
-    assert ('does not exist' in result.output)
+    assert ('No such file or directory' in result.output)
 
 
 @pytest.mark.parametrize('proc_crs', [ProcCrs.auto, ProcCrs.ref, ProcCrs.src])
