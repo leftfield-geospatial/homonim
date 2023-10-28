0ac724090bd9049a4b80fcca64da1548dac30eeb
Dugal Harris <dugalh@gmail.com>
2023-10-28 19:13:30 +0200

    remove _static & update copyright

--- docs/conf.py
+++ docs/conf.py
@@ -34,8 +34,8 @@ from homonim.version import __version__
 # -- Project information -----------------------------------------------------
 
 project = 'homonim'
-copyright = '2022, Dugal Harris'
-author = 'Dugal Harris'
+copyright = '2022-2023, Leftfield Geospatial'
+author = 'Leftfield Geospatial'
 
 # The full version, including alpha/beta/rc tags
 release = __version__
@@ -78,7 +78,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 # -- Options for autodoc -----------------------------------------------------
 # see https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html
